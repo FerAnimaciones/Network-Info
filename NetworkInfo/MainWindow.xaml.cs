59022b98efece4bd4cf52ddcf7b7ae46aadf33ad
Fernando Manuel Avila Cataño <feranimaciones@gmail.com>
2021-10-29 10:56:33 -0600

    Act

--- NetworkInfo/MainWindow.xaml.cs
+++ NetworkInfo/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,41 @@ namespace NetworkInfo
         public MainWindow()
         {
             InitializeComponent();
+            if (System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+               txtIp.Content= GetLocalIPAddress();
+               txtMAC.Content= GetMAC();
+            }
+        }
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        public static string GetMAC()
+        {
+            var macAddr =(from nic in NetworkInterface.GetAllNetworkInterfaces()where nic.OperationalStatus == OperationalStatus.Up select nic.GetPhysicalAddress().ToString()      ).FirstOrDefault();
+            return macAddr.ToString().InsertCharAtDividedPosition(2, "-"); ;
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+    public static class StringExtenstions
+    {
+        public static string InsertCharAtDividedPosition(this string str, int count, string character)
+        {
+            var i = 0;
+            while (++i * count + (i - 1) < str.Length)
+            {
+                str = str.Insert((i * count + (i - 1)), character);
+            }
+            return str;
         }
     }
 }
